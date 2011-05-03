4f02a0ec70fc3841bc604140b79bf3661ed21836
arturo <arturo@openframeworks.cc>
2011-05-03 02:12:01 +0200

    OFActivity: new activity with menu support

--- srcJava/cc/openframeworks/androidFerns/OFActivity.java
+++ srcJava/cc/openframeworks/androidFerns/OFActivity.java
@@ -2,9 +2,9 @@ package cc.openframeworks.androidFerns;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.KeyEvent;
-import android.view.Window;
+import android.view.Menu;
+import android.view.MenuItem;
 import cc.openframeworks.OFAndroid;
 
 
@@ -15,7 +15,6 @@ public class OFActivity extends Activity{
     { 
         super.onCreate(savedInstanceState);
         String packageName = getPackageName();
-
         ofApp = new OFAndroid(packageName,this);
     }
 	
@@ -52,8 +51,39 @@ public class OFActivity extends Activity{
         return super.onKeyUp(keyCode, event);
     }
 
-	
+
 	OFAndroid ofApp;
+    
+	
+	
+    // Menus
+    // http://developer.android.com/guide/topics/ui/menus.html
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	// Create settings menu options from here, one by one or infalting an xml
+        return super.onCreateOptionsMenu(menu);
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	// This passes the menu option string to OF
+    	// you can add additional behavior from java modifying this method
+    	// but keep the call to OFAndroid so OF is notified of menu events
+    	if(OFAndroid.menuItemSelected(item)){
+    		
+    		return true;
+    	}
+    	return super.onOptionsItemSelected(item);
+    }
+    
+
+    @Override
+    public boolean onPrepareOptionsMenu (Menu menu){
+    	// This method is called every time the menu is opened
+    	//  you can add or remove menu options from here
+    	return  super.onPrepareOptionsMenu(menu);
+    }
+	
 }
 
 

8f0c58660041035a6232156c7435951fa429bebe
damian at artvertiserredo1 <damian@frey.co.nz>
2012-04-18 18:29:39 +0200

    override width and height of detector

--- src/ArtvertiserApp.cpp
+++ src/ArtvertiserApp.cpp
@@ -28,8 +28,12 @@
 
 //void ofSoundShutdown(){};
 
-int camW = 640;
-int camH = 480;
+int camW = 800;
+int camH = 600;
+int detectW = 640;
+int detectH = 480;
+//int detectW = -1;
+//int detectH = -1;
 
 //static const string SERVER_URL = "http://192.168.1.134:8888";
 static const string SERVER_URL = "http://localhost:8888";
@@ -39,6 +43,7 @@ static const string SERVER_URL = "http://localhost:8888";
 void ArtvertiserApp::setup(){
 	//ofSleepMillis(5000);
 	ofSetVerticalSync(true);
+//	ofSetFrameRate( 30.0f );
 	//ofSetLogLevel(OF_LOG_VERBOSE);
 	ofBackground(66,51,51);
 	ofEnableAlphaBlending();
@@ -71,6 +76,29 @@ void ArtvertiserApp::setup(){
 	grabber.setUseTexture(false);
 	//grabber.setPixelFormat(OF_PIXELS_MONO);
 	grabber.initGrabber(camW, camH);
+	#ifdef TARGET_LINUX
+		ofLogNotice("ArtvertiserApp", "sleeping 10s to allow camera to recover");
+	// sleep to allow the camera to recover from init
+		sleep(10);
+	#endif
+
+
+
+	// drop the first few frames
+	int dropFrameCount = 0;
+	while ( dropFrameCount < 10 )
+	{
+		grabber.update();
+		if ( !grabber.isFrameNew() )
+			usleep( 1000*10 );
+		else
+		{
+	//		const ofPixels& p = grabber.getPixelsRef();
+			ofLogVerbose("ArtvertiserApp") << "dropping frame "/* << p.getWidth() << "x" << p.getHeight()*/;
+			dropFrameCount++;
+		}
+	}
+	
 	
 
 	if ( CommandlineParser::get()->isRunningOnBinoculars() )
@@ -353,11 +381,11 @@ void ArtvertiserApp::artvertSelected(ofFile & artvertimg){
 	Artvert & artvert = artvertInfo.getCurrentArtvert();
 
 	if(artvert.hasAlias()){
-		artvertiser.setup(artvert.getAlias().getModel().getAbsolutePath(),grabber,imgQuad);
+		artvertiser.setup(artvert.getAlias().getModel().getAbsolutePath(),grabber,imgQuad, false, detectW, detectH );
 		ofLogVerbose("ArtvertiserApp", "artvert.hasAlias()");
 	}else{
 		ofLogVerbose("ArtvertiserApp", "doesn't: artvert.hasAlias()");
-		artvertiser.setup(artvert.getModel().getAbsolutePath(),grabber,imgQuad);
+		artvertiser.setup(artvert.getModel().getAbsolutePath(),grabber,imgQuad, false, detectW, detectH );
 	}
 }
 
@@ -384,11 +412,11 @@ void ArtvertiserApp::artvertSelectedBinoculars(Binocular::ArtvertSelectedEventIn
 	Artvert & artvert = info.selectedAdvert;
 	if ( artvert.hasAlias() )
 	{
-		artvertiser.setup(artvert.getAlias().getModel().getAbsolutePath(),grabber,imgQuad);
+		artvertiser.setup(artvert.getAlias().getModel().getAbsolutePath(),grabber,imgQuad, false, detectW, detectH );
 		ofLogVerbose("ArtvertiserApp", "artvert.hasAlias()");
 	}else{
 		ofLogVerbose("ArtvertiserApp", "doesn't: artvert.hasAlias()");
-		artvertiser.setup(artvert.getModel().getAbsolutePath(),grabber,imgQuad);
+		artvertiser.setup(artvert.getModel().getAbsolutePath(),grabber,imgQuad, false, detectW, detectH );
 	}
 }
 
